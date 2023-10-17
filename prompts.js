6c5fac63ead6cc2ea1dbe530b91990f0bdbd61b5
jackomix <jacklack33@outlook.com>
2023-10-17 14:54:05 -0400

    cleaned up prompts.js

--- prompts.js
+++ prompts.js
@@ -10,22 +10,7 @@ ${prompt}
 partyMembers:`
 }
 
-function rulesPrompt() {
-    return `- Add multiple strings into the "newChoices" array. 
-- Modify "inventory", "partyMembers", party member's "bars" and "emotion"s as necessary. 
-- Keep the inventory current and remove unusable items. 
-
-Return the YAML object.`
-}
-
 function actionPrompt(stats) {
-    /*return `Goal: Continue the story with what's happening next after the player's "lastChoice" 
-- Write what happens next in "newStoryBeat". Go further and be extraordinary creative!
-- Write multiple new choices the player must choose in "newChoices". 
-- Write and update "storyRecap" and "lastStoryBeat". 
-${rulesPrompt()}
-
-${jsyaml.dump(stats)}`*/
     return `Choose-Your-Own-Adventure Game:
 
 - Continue the game and story based on the player's "lastChoice."
